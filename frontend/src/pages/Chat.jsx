6e1ef6a3bbfcafaa847b9b2035dfade65c980c9e
tomse <tomset111@gmail.com>
2024-03-01 15:53:32 +0100

    Due to how setInterval works, explicitely fetch users and chats in the beginning of useEffect responsible for fetching those data.

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -56,6 +56,9 @@ const Chat = () => {
     }
 
     useEffect(() => {
+        fetchUsers();
+        fetchChats();
+
         const fetchUsersInterval = setInterval(fetchUsers, 10000);
         const fetchChatsInterval = setInterval(fetchChats, 10000);
 
